9bc5dd41c063e969c7a49444bba93ab705c8e846
SantiSanGal <santiagon.g.m@gmail.com>
2024-07-07 01:44:51 -0400

    Fin Sección 2

--- 01-socket-server-basic/models/sockets.js
+++ 01-socket-server-basic/models/sockets.js
@@ -13,7 +13,7 @@ class Sockets {
             socket.on('mensaje-to-server', (data) => {
                 //Cuando utilizo Socket emite solo al id al que se conectó el nuevo cliente
                 // socket.emit('mensaje-from-server', data)
-
+                console.log(data);
                 //Si quiero enviar a todos los clientes, debo utilizar io.emit
                 this.io.emit('mensaje-from-server', data) //envía a un Namespace
             })
